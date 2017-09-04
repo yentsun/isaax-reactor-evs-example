afc21715e0ca54eb6cbe5029d6ab49c38d23485d
Maxim Korinets <mkorinets@gmail.com>
2017-09-04 08:42:08 +0700

    ADDED http server

--- service.js
+++ service.js
@@ -1,11 +1,12 @@
-const Reactor = require('isaax-reactor');
 const stan = require('node-nats-streaming').connect('test-cluster', 'subscriber');
+const Reactor = require('isaax-reactor');
+const express = require('express');
 const pack = require('./package.json');
-
 
 
 const reactor = new Reactor('evs');
 let c;
+const app = express();
 
 stan.on('error', (error) => {
    console.log(error.message);
@@ -24,6 +25,19 @@ reactor.on('config', (config, done) => {
 });
 
 reactor.on('ready', () => {
+    const store = reactor.get('store');
+    app.get('/', function (req, res) {
+        res.json({total: store.total, list: store.list});
+    });
+    app.get('/:id', function (req, res) {
+        const {id} = req.params;
+        res.json(store.ids[id]);
+    });
+    app.listen(reactor.config.http.port, function () {
+        console.log('http listening on', reactor.config.http.port)
+    });
+
+
     console.log(`************************************************************************`);
     console.log(`  ${c.pack.name}@${c.pack.version} ready [${c.environment}]`);
     console.log(`************************************************************************`);
