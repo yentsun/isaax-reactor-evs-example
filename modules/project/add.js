05c7723660c498826746e0cc7e6950f595d484d8
Maxim Korinets <mkorinets@gmail.com>
2017-09-04 09:00:50 +0700

    ADDED `loggedAt` argument

--- modules/project/add.js
+++ modules/project/add.js
@@ -1,9 +1,10 @@
 module.exports = (reactor, logger) => {
 
-    return (seq, projectData, done=()=>{}) => {
+    return (seq, loggedAt, projectData, done=()=>{}) => {
         const store = reactor.get('store');
         projectData.title = `Title #${projectData.id}`;
         projectData.seq = seq;
+        projectData.loggedAt = loggedAt;
         store.list.push(projectData.id);
         store.total++;
         store.ids[projectData.id] = projectData;
--- subscribers/project.add.js
+++ subscribers/project.add.js
@@ -1,14 +1,12 @@
 module.exports = (reactor, logger) => {
     const stan = reactor.get('stan');
-    const store = reactor.get('store');
     const project = reactor.module('project');
     const opts = stan.subscriptionOptions().setDeliverAllAvailable();
     const sub = stan.subscribe('project.add', opts);
     sub.on('message', (msg) => {
         logger.debug('got message', msg);
         const seq = msg.getSequence();
-        project.add(seq, JSON.parse(msg.getData()), () => {
-            console.log(msg.getTimestamp(), store.list.length);
-        });
+        const loggedAt = msg.getTimestamp();
+        project.add(seq, loggedAt, JSON.parse(msg.getData()));
     });
 };
